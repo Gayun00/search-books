314f8a0189e9092f7ac80902f4718cb8366865d7
Gayun00 <gayunlee11@gmail.com>
2023-12-07 00:29:23 +0900

    Chore: 공통 api 호출 로직 작성

---
+++ src/utils/apiRequest/index.ts
@@ -0,0 +1,43 @@
+import { API_SERVER } from "@/constants";
+
+interface RequestParams<TParams> {
+  path: string;
+  params?: TParams;
+  method?: "get" | "post" | "put" | "delete";
+}
+
+const headers: HeadersInit = {
+  "Content-Type": "application/json; charset=utf-8",
+};
+
+export const apiRequest = <TParams>({
+  path,
+  method,
+  params,
+}: RequestParams<TParams>) => {
+  return fetch(`${API_SERVER}${path}`, {
+    headers,
+    method,
+    body: JSON.stringify(params),
+  }).then((res) => res.json());
+};
+
+export const request = {
+  get<TParams, TResponse>(params: RequestParams<TParams>): Promise<TResponse> {
+    return apiRequest<TParams>({ ...params, method: "get" });
+  },
+
+  post<TParams, TResponse>(params: RequestParams<TParams>): Promise<TResponse> {
+    return apiRequest({ ...params, method: "post" });
+  },
+
+  put<TParams, TResponse>(params: RequestParams<TParams>): Promise<TResponse> {
+    return apiRequest({ ...params, method: "put" });
+  },
+
+  delete<TParams, TResponse>(
+    params: RequestParams<TParams>
+  ): Promise<TResponse> {
+    return apiRequest({ ...params, method: "delete" });
+  },
+};
