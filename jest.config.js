b82470b1d6b1afd523f48e0dbf25aa56ece58956
Gayun00 <gayunlee11@gmail.com>
2023-12-06 20:08:43 +0900

    Chore: husky test, lint-staged precommit 설정

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   collectCoverage: true,
-  // on node 14.x coverage provider v8 offers good speed and more or less good report
   coverageProvider: "v8",
   collectCoverageFrom: [
     "**/*.{js,jsx,ts,tsx}",
@@ -12,22 +11,12 @@ module.exports = {
     "!<rootDir>/src/coverage/**",
   ],
   moduleNameMapper: {
-    // Handle CSS imports (with CSS modules)
-    // https://jestjs.io/docs/webpack#mocking-css-modules
     "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-
-    // Handle CSS imports (without CSS modules)
     "^.+\\.(css|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
-
-    // Handle image imports
-    // https://jestjs.io/docs/webpack#handling-static-assets
     "^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i": `<rootDir>/src/__mocks__/fileMock.js`,
-
-    // Handle module aliases
     "^@/components/(.*)$": "<rootDir>/src/components/$1",
     "^@/utils/(.*)$": "<rootDir>/src/utils/$1",
   },
-  // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/src/utils/testUtils/setup.ts"],
   testPathIgnorePatterns: [
     "<rootDir>/src/node_modules/",
@@ -35,8 +24,6 @@ module.exports = {
   ],
   testEnvironment: "jsdom",
   transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
   transformIgnorePatterns: [
