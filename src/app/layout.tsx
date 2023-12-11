a3b13cf6b9a186e333f25c214504a3b205b89fb4
Gayun00 <gayunlee11@gmail.com>
2023-12-12 00:51:00 +0900

    Chore: providers 폴더 생성
    - 컴포넌트 형태로 감싸는 Wrapper 컴포넌트를 모아둘 수 있도록 별도 폴더 생성해 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Provider from "@/components/Provider";
+import Provider from "@/components/providers/Provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
