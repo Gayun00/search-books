6a558fe94d772942d64baf98f7015f751a370eb1
Gayun00 <gayunlee11@gmail.com>
2023-12-11 20:39:44 +0900

    Refactor: query wrapping 함수 선언 및 로직 분리
    - 컴포넌트 내에서 쿼리 랩핑 함수 호출하도록 분리

--- src/components/SearchBooks/index.tsx
+++ src/components/SearchBooks/index.tsx
@@ -1,68 +1,18 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { BookData } from "@/types";
 import BookList from "@/components/BookList";
 import SearchInput from "@/components/SearchInput";
-import { searchBooks } from "@/api";
-import { splitKeywords } from "@/utils/handleKeywords";
 import useIntersectionObserver from "@/hooks/useIntersectionObserver";
 import Book from "@/components/Book";
 import NoData from "../fallbacks/NoData";
 import Spinner from "../fallbacks/Spinner";
+import { useSearchBooksQuery } from "@/queries";
 
 function SearchBooks() {
   const [keywords, setKeywords] = useState("");
 
   const { data, fetchNextPage, isLoading, hasNextPage, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: ["searchBooks", keywords],
-      queryFn: async ({ pageParam }) => {
-        const hasOrOperator = keywords.includes("|");
-        const hasNotOperator = keywords.includes("-");
-
-        if (hasOrOperator) {
-          const keywordArray = splitKeywords(keywords);
-          const promises = keywordArray.map((keyword) => {
-            return searchBooks({ keyword, page: pageParam });
-          });
-
-          return Promise.all(promises).then((results) => {
-            const mergedResults = results.flatMap((result) => result.books);
-            return {
-              books: mergedResults,
-              page: pageParam,
-              total: results[pageParam].total,
-            };
-          });
-        }
-
-        if (hasNotOperator) {
-          const [keyword1, keyword2] = splitKeywords(keywords);
-          const searchedBooks = await searchBooks({
-            keyword: keyword1,
-            page: pageParam,
-          });
-          const filteredBooks = searchedBooks.books.filter(
-            (book) => !book.title.toLowerCase().includes(keyword2.toLowerCase())
-          );
-          return {
-            books: filteredBooks,
-            page: pageParam,
-            total: searchedBooks.total,
-          };
-        }
-
-        return searchBooks({ keyword: keywords, page: pageParam });
-      },
-      getNextPageParam: (lastPage: any) => {
-        const nextPage = parseInt(lastPage.page) + 1;
-        const totalItems = parseInt(lastPage.total);
-        if (nextPage * 10 <= totalItems) {
-          return nextPage;
-        }
-      },
-      initialPageParam: 1,
-    });
+    useSearchBooksQuery({ keywords });
 
   const hasNoSearchResults = !data?.pages[0].books.length;
   const hasSearchResults = !!(!isLoading && keywords.length);
---
+++ src/queries/index.ts
@@ -0,0 +1,62 @@
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { getBook, searchBooks } from "@/api";
+import { splitKeywords } from "@/utils/handleKeywords";
+
+export const useSearchBooksQuery = ({ keywords }: { keywords: string }) => {
+  return useInfiniteQuery({
+    queryKey: ["searchBooks", keywords],
+    queryFn: async ({ pageParam }) => {
+      const hasOrOperator = keywords.includes("|");
+      const hasNotOperator = keywords.includes("-");
+
+      if (hasOrOperator) {
+        const keywordArray = splitKeywords(keywords);
+        const promises = keywordArray.map((keyword) => {
+          return searchBooks({ keyword, page: pageParam });
+        });
+
+        return Promise.all(promises).then((results) => {
+          const mergedResults = results.flatMap((result) => result.books);
+          return {
+            books: mergedResults,
+            page: pageParam,
+            total: results[pageParam].total,
+          };
+        });
+      }
+
+      if (hasNotOperator) {
+        const [keyword1, keyword2] = splitKeywords(keywords);
+        const searchedBooks = await searchBooks({
+          keyword: keyword1,
+          page: pageParam,
+        });
+        const filteredBooks = searchedBooks.books.filter(
+          (book) => !book.title.toLowerCase().includes(keyword2.toLowerCase())
+        );
+        return {
+          books: filteredBooks,
+          page: pageParam,
+          total: searchedBooks.total,
+        };
+      }
+
+      return searchBooks({ keyword: keywords, page: pageParam });
+    },
+    getNextPageParam: (lastPage: any) => {
+      const nextPage = parseInt(lastPage.page) + 1;
+      const totalItems = parseInt(lastPage.total);
+      if (nextPage * 10 <= totalItems) {
+        return nextPage;
+      }
+    },
+    initialPageParam: 1,
+  });
+};
+
+export const useBookQuery = ({ isbn13 }: { isbn13: string }) => {
+  return useQuery({
+    queryKey: ["book"],
+    queryFn: () => getBook({ isbn13 }),
+  });
+};
