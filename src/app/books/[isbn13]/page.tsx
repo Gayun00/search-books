651c4d76552c28c5eeab9a7fd520def9623f85c6
Gayun00 <gayunlee11@gmail.com>
2023-12-11 19:46:54 +0900

    Add: isbn13으로 fetch한 도서 정보 표시

--- src/app/books/[isbn13]/page.tsx
+++ src/app/books/[isbn13]/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { getBook } from "@/api";
+import HydrateOnClient from "@/hooks/hydrateOnClient";
+import getQueryClient from "@/utils/getQueryClient";
+import { dehydrate } from "@tanstack/react-query";
+import Client from "./Client";
 
-function Page({ params }: { params: { isbn13: string } }) {
-  return <div>{params.isbn13}</div>;
+interface Props {
+  params: { isbn13: string };
+}
+
+async function Page({ params }: Props) {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["book", params.isbn13],
+    queryFn: () => getBook({ isbn13: params.isbn13 }),
+  });
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrateOnClient state={dehydratedState}>
+      <Client isbn13={params.isbn13} />
+    </HydrateOnClient>
+  );
 }
 
 export default Page;
