c1e7fcbf7773e206d96a09439d5d7edd313c4ec0
gygy <67543454+Gayun00@users.noreply.github.com>
2023-12-11 16:27:52 +0900

    Revert "Add: 도서 검색 리스트 페이지 추가 (#16)" (#17)
    This reverts commit 33a7dece630984bd1311d788dc9a02a9c0abc8b5.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-"use client";
-
-import { searchBooks } from "@/api";
-import SearchBooks from "@/components/SearchBooks";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <SearchBooks />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     Home
     </main>
-  );
+  )
 }
