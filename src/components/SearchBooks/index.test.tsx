72555fb0bdcf5e4b41982f50fba19dfb1269371b
Gayun00 <gayunlee11@gmail.com>
2023-12-11 14:14:16 +0900

    Add: testUtils query wrapper custom render 함수 추가
    - 테스트 환경에서 react-query를 사용할 수 있도록 custom render 함수 추가

--- src/components/SearchBooks/index.test.tsx
+++ src/components/SearchBooks/index.test.tsx
@@ -1,6 +1,7 @@
-import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import { act, fireEvent, waitFor } from "@testing-library/react";
 import * as api from "@/api";
 import SearchBooks from ".";
+import { customRender } from "@/utils/testUtils";
 
 jest.mock("@/api", () => ({
   searchBooks: jest.fn().mockImplementation(({ keyword }) => {
@@ -23,7 +24,7 @@ jest.mock("@/api", () => ({
 
 describe("SearchBooks 테스트", () => {
   it("하나의 키워드만 입력하면 해당 키워드의 검색 결과를 표시한다", async () => {
-    const { getByText, getByPlaceholderText } = render(<SearchBooks />);
+    const { getByText, getByPlaceholderText } = customRender(<SearchBooks />);
     const inputField = getByPlaceholderText("검색어를 입력하세요");
     const submitButton = getByText("검색");
 
@@ -40,7 +41,7 @@ describe("SearchBooks 테스트", () => {
   });
 
   it("| 연산자를 포함한 두 개의 키워드를 입력하면 각각의 키워드 검색 결과를 합쳐 표시한다", async () => {
-    const { getByText, getByPlaceholderText } = render(<SearchBooks />);
+    const { getByText, getByPlaceholderText } = customRender(<SearchBooks />);
     const inputField = getByPlaceholderText("검색어를 입력하세요");
     const submitButton = getByText("검색");
 
@@ -58,7 +59,7 @@ describe("SearchBooks 테스트", () => {
   });
 
   it("- 연산자를 포함해 두 개의 키워드를 입력하면 첫번째 키워드 검색 결과에서 두 번째 키워드를 포함한 제목을 갖는 결과를 제외해 표시한다", async () => {
-    const { getByText, queryByText, getByPlaceholderText } = render(
+    const { getByText, queryByText, getByPlaceholderText } = customRender(
       <SearchBooks />
     );
     const inputField = getByPlaceholderText("검색어를 입력하세요");
